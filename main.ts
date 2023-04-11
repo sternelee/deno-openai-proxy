b66c4880783f07b297b72613182e4ea766326936
李兴广 <lixingguang@xunlei.com>
2023-04-11 12:50:06 +0800

    feat: 只返回openid

--- main.ts
+++ main.ts
@@ -8,52 +8,61 @@ import { createParser } from "https://esm.sh/eventsource-parser@1.0.0";
 const OPENAI_API_HOST = "api.openai.com";
 // const OPENAI_API_HOST = "lee-chat.deno.dev";
 const APIKEY = Deno.env.get("OPEN_AI_KEY");
-const APPID = Deno.env.get("APPID");
-const SECRET = Deno.env.get("SECRET");
-const MAX_DAY_COUNT = 3
-const MY_KEY = 'l5e2e0'
+const APPID = Deno.env.get("APPID") || "";
+const SECRET = Deno.env.get("SECRET") || "";
+const MAX_DAY_COUNT = 3;
+const MY_KEY = "l5e2e0";
 
 const clients = new Map();
-const users : {
+const users: {
   [openid: string]: {
     day: string;
     count: number;
-  }
-}= {};
+  };
+} = {};
 
 const getDayCount = (openid: string) => {
-  const now = new Date().toLocaleDateString()
+  const now = new Date().toLocaleDateString();
   if (users[openid] && users[openid].day === now) {
-    if (users[openid].count >= MAX_DAY_COUNT) return 0
-    users[openid].count += 1
-    return users[openid].count
+    if (users[openid].count >= MAX_DAY_COUNT) return 0;
+    users[openid].count += 1;
+    return users[openid].count;
   } else {
     users[openid] = {
       day: now,
-      count: 1
-    }
-    return 1
+      count: 1,
+    };
+    return 1;
   }
-}
+};
 
 const decoder = new TextDecoder();
 
 serve(async (request: Request) => {
   const url = new URL(request.url);
   const upgrade = request.headers.get("upgrade") || "";
-  console.log('URL:', url.pathname)
-  console.log('upgrade:', upgrade)
+  console.log("URL:", url.pathname);
   if (upgrade.toLowerCase() != "websocket") {
     if (url.pathname === "/") {
       return fetch(new URL("./Readme.md", import.meta.url));
     }
 
     if (url.pathname === "/jscode2session") {
-      return await fetch(
-        `https://api.weixin.qq.com/sns/jscode2session?js_code=${url.searchParams.get(
-          "js_code"
-        )}&appid=${APPID}&secret=${SECRET}&grant_type=authorization_code`
-      );
+      const ret = await fetch(
+        `https://api.weixin.qq.com/sns/jscode2session?js_code=${
+          url.searchParams.get(
+            "js_code",
+          )
+        }&appid=${APPID}&secret=${SECRET}&grant_type=authorization_code`,
+      ).then((response) => response.json());
+      if (ret.openid) {
+        return new Response(JSON.stringify(
+          {
+            openid: ret.openid,
+          },
+        ));
+      }
+      return new Response(JSON.stringify(ret));
     }
 
     url.host = OPENAI_API_HOST;
@@ -62,7 +71,9 @@ serve(async (request: Request) => {
 
   const { socket, response } = Deno.upgradeWebSocket(request);
   const openid = url.pathname.split("/ws/")[1];
-  socket.onopen = () => console.log("socket opened");
+  socket.onopen = () => {
+    // console.log("socket opened")
+  };
   if (openid && !clients.get(openid)) {
     clients.set(openid, socket);
   }
@@ -74,7 +85,9 @@ serve(async (request: Request) => {
       const client = clients.get(openid) || socket;
       if (!client) return;
       if (type === "chat") {
-        const auth = key.includes(MY_KEY) && getDayCount(openid) > 0 ? APIKEY : key;
+        const auth = key.includes(MY_KEY) && getDayCount(openid) > 0
+          ? APIKEY
+          : key;
         const url = `https://${OPENAI_API_HOST}${action}`;
         const rawRes = await fetch(url, {
           headers: {
@@ -93,7 +106,7 @@ serve(async (request: Request) => {
               type: "fail",
               status: 500,
               message: err.message,
-            })
+            }),
           );
         });
 
@@ -104,7 +117,7 @@ serve(async (request: Request) => {
               type: "fail",
               status: rawRes.status,
               message: rawRes.statusText,
-            })
+            }),
           );
         }
         const streamParser = (event: ParsedEvent | ReconnectInterval) => {
@@ -117,7 +130,7 @@ serve(async (request: Request) => {
               const json = JSON.parse(data);
               const text = json.choices[0].delta?.content;
               client.send(
-                JSON.stringify({ type: "ok", status: 200, content: text })
+                JSON.stringify({ type: "ok", status: 200, content: text }),
               );
             } catch (e) {
               client.send(
@@ -125,7 +138,7 @@ serve(async (request: Request) => {
                   type: "fail",
                   status: 200,
                   content: e.message.toString(),
-                })
+                }),
               );
             }
           }
@@ -141,9 +154,13 @@ serve(async (request: Request) => {
       socket.send(e.message);
     }
   };
-  socket.onerror = (e) => console.log("socket errored:", e);
+  socket.onerror = (e) => {
+    console.log("socket errored:", e);
+    if (openid && clients.get(openid)) {
+      clients.delete(openid);
+    }
+  };
   socket.onclose = () => {
-    console.log("socket closed");
     if (openid && clients.get(openid)) {
       clients.delete(openid);
     }
